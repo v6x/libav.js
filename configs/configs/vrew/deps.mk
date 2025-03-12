f88e5a4ff9afb6591258890701b202c0ae23a646
Sojeong Lee <sojeong.lee@voyagerx.com>
2025-03-12 13:21:53 +0900

    Add mp3 related configs

--- configs/configs/vrew/deps.mk
+++ configs/configs/vrew/deps.mk
@@ -1,2 +1,4 @@
 build/ffmpeg-$(FFMPEG_VERSION)/build-base-vrew/ffbuild/config.mak: build/inst/base/lib/pkgconfig/openh264.pc
 build/ffmpeg-$(FFMPEG_VERSION)/build-thr-vrew/ffbuild/config.mak: build/inst/thr/lib/pkgconfig/openh264.pc
+build/ffmpeg-$(FFMPEG_VERSION)/build-base-vrew/ffbuild/config.mak: build/inst/base/lib/libmp3lame.a
+build/ffmpeg-$(FFMPEG_VERSION)/build-thr-vrew/ffbuild/config.mak: build/inst/thr/lib/libmp3lame.a
